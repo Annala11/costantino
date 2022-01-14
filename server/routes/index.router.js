661a7ce18ef41b5eaf538f0e617d44d9d111bf1e
Anna <annala1109@gmail.com>
2022-01-14 16:21:06 +0300

    fix route names

--- server/routes/index.router.js
+++ server/routes/index.router.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
 const loginRouter = require('./login.router');
 const logoutRouter = require('./logout.router');
-const registrationRouter = require('./registration.route');
-const sessionRouter = require('./session.route');
+const registrationRouter = require('./registration.router');
+const sessionRouter = require('./session.router');
 const reviewsRouter = require('./reviews.router');
 const categoriesRouter = require('./categories.router');
 const servicesRouter = require('./services.router');
-const profileRouter = require('./profile.route');
-const adminRouter = require('./admin.route');
-const scheduleRouter = require('./schedule.route');
-const isAuthRouter = require('./isAuth.route');
-const stockpriceRouter = require('./stockprice.route');
+const profileRouter = require('./profile.router');
+const adminRouter = require('./admin.router');
+const scheduleRouter = require('./schedule.router');
+const isAuthRouter = require('./isAuth.router');
+const stockpriceRouter = require('./stockprice.router');
 
 
 router.use('/login', loginRouter);
