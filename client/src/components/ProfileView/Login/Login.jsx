dd62c708636ab8f0b56375829c87a566e1206389
lexu <alexey.uchkin@gmail.com>
2022-01-15 20:56:32 +0300

    ymaps homepage

--- client/src/components/ProfileView/Login/Login.jsx
+++ client/src/components/ProfileView/Login/Login.jsx
@@ -11,18 +11,11 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useHistory } from 'react-router';
 import { useDispatch } from 'react-redux';
 import { userLoginAC } from '../../../redux/ActionCreators/usersAC';
 
-
-
-
-
 function Login() {
-
-  const theme = createTheme();
 
   const dispatch = useDispatch();
   let history = useHistory();
@@ -53,7 +46,6 @@ function Login() {
       })
   };
   return (
-    <ThemeProvider theme={theme}>
       <Grid container component="main" sx={{ height: '100vh' }}>
         <CssBaseline />
         <Grid
@@ -137,7 +129,6 @@ function Login() {
           </Box>
         </Grid>
       </Grid>
-    </ThemeProvider>
   );
 }
 
