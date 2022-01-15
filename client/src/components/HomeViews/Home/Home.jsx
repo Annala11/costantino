1f26086bd68189020fa3a8217b84c3279bcc60cf
layabout <minyaeavmail@gmail.com>
2022-01-15 17:53:57 +0300

    baseRouting and appBar

--- client/src/components/HomeViews/Home/Home.jsx
+++ client/src/components/HomeViews/Home/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import HomeBilboard from '../HomeBilboard/HomeBilboard';
+import HomeServicesAndPrices from '../HomeServicesAndPrices/HomeServicesAndPrices';
+import Maps from '../Maps/Maps';
+import HomeReviews from '../HomeReviews/HomeReviews';
+import Footer from '../Footer/Footer'
 
 function Home() {
   return (
-    <div>
-      It is Home component!   
-    </div>
+    <>
+      <HomeBilboard />
+      <HomeServicesAndPrices />
+      <Maps />
+      <HomeReviews />
+      <Footer />
+    </>
   );
 }
 
--- client/src/components/HomeViews/Navigation/Navigation.jsx
+++ client/src/components/HomeViews/Navigation/Navigation.jsx
@@ -1,47 +1,166 @@
 import React from 'react';
 
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
+import { Toolbar, IconButton, Typography, Button } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import CallIcon from '@mui/icons-material/Call';
+import { styled, useTheme } from '@mui/material/styles';
 
-
-// import { styled, useTheme } from '@mui/material/styles';
-// import Box from '@mui/material/Box';
-// import Drawer from '@mui/material/Drawer';
-// import CssBaseline from '@mui/material/CssBaseline';
-// import MuiAppBar from '@mui/material/AppBar';
-// import List from '@mui/material/List';
-// import Divider from '@mui/material/Divider';
-// import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-// import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-// import ListItem from '@mui/material/ListItem';
-// import ListItemIcon from '@mui/material/ListItemIcon';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
 // import ListItemText from '@mui/material/ListItemText';
-// import InboxIcon from '@mui/icons-material/MoveToInbox';
-// import MailIcon from '@mui/icons-material/Mail';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
 
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }),
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
 
 function Navigation() {
 
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <AppBar position="fixed" color='primary' style={{background: `linear-gradient(45deg, #13547a, #80d0c7)`, color:'#FFE4B5', textShadow: `1px 1px 2px pink`}}>
-      <Toolbar>
-        <IconButton edge="start"
-          color="inherit">
-          <MenuIcon />
-          {/* <div>
-      <Link to='/'> Home </Link>
-    </div> */}
-        </IconButton>
-        <Typography variant="h6" style={{ flexGrow: 1 }}> Costantino </Typography>
-        <CallIcon></CallIcon>
-        <Button color="inherit" size='small'>
-          8-812-777-777-77
-        </Button>
-      </Toolbar>
-    </AppBar>
+
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}
+        style={{
+          background: `linear-gradient(45deg, #13547a, #80d0c7)`,
+          color: '#FFE4B5',
+          textShadow: `1px 1px 2px pink`
+        }} >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="end"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div" style={{ flexGrow: 1 }}>
+            Costantino
+          </Typography>
+          <CallIcon></CallIcon>
+          <Button color="inherit" size='small'>
+            8-812-777-777-77
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader style={{
+          background: `linear-gradient(45deg, #13547a, #80d0c7)`,
+          color: '#FFE4B5',
+          textShadow: `1px 1px 2px pink`
+        }}>
+          <Typography variant="h6" noWrap component="div" style={{ flexGrow: 1 }}>
+            Costantino
+          </Typography>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List style={{
+          background: `linear-gradient(45deg, #13547a, #80d0c7)`,
+          color: '#FFE4B5',
+          textShadow: `1px 1px 2px pink`
+        }}>
+          {[
+            {text:'ЗАПИСАТЬСЯ ОН-ЛАЙН', link: '/neworder'},
+            {text:'УСЛУГИ И ЦЕНЫ', link: '/servicesandprices'},
+            {text:'АКЦИИ', link:'/stockprice'},
+            // Добавить контакты и условное форматирование в зависимости от сессии! ! ! ! ! !  !
+          ].map((el, index) => (
+            <ListItem button key={el.text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <Link to ={el.link} > {el.text} </Link>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+      </Main>
+    </Box>
+
   );
 }
 
