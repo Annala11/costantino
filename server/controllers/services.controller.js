c3819e11e7a96b52a58727fc74e217f88d30e82a
Anna <annala1109@gmail.com>
2022-01-15 19:53:33 +0300

    create route for services

---
+++ server/controllers/services.controller.js
@@ -0,0 +1,14 @@
+const { Service } = require('../db/models');
+
+const allServices = async (req, res) => {
+  try {
+    const services = await Service.findAll({
+      order: [['id', 'ASC']],
+    });
+    res.status(200).json(services);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+module.exports = { allServices };
