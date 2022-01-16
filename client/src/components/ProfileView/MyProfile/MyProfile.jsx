3b15e29df3d175d404e0e052633a610fec4e24cb
Anna <annala1109@gmail.com>
2022-01-16 22:59:24 +0300

    start create profile

--- client/src/components/ProfileView/MyProfile/MyProfile.jsx
+++ client/src/components/ProfileView/MyProfile/MyProfile.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+// import { useHistory } from 'react-router';
 
 function MyProfile() {
+
+  // const history = useHistory();
+
+  const user = useSelector(state => state.admin);
+  //TODO
   return (
     <div>
-      MyProfile   
+      <div>Имя: {user.name}</div>
+      <div>Телефон: {user.phone}</div>
     </div>
   );
 }
