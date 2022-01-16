1624713f4493ef0fba254b1b02b27960a020a3ed
Anna <annala1109@gmail.com>
2022-01-17 00:16:33 +0300

    add attributes to sql query

--- server/controllers/reviews.controller.js
+++ server/controllers/reviews.controller.js
@@ -3,13 +3,13 @@ const { Review } = require('../db/models');
 const allReviews = async (req, res) => {
   try {
     const reviews = await Review.findAll({
-      order: [['id', 'ASC']],
+      attributes: ['id', 'title', 'text', 'rating', 'branch_id', 'user_id']
     });
-    console.log(reviews);
-    res.status(200).json({ reviews });
+    // console.log({ reviews });
+    res.status(200).json(reviews);
   } catch (error) {
     res.status(404).json({ error: error.message });
   }
 
 }
-module.exports = {allReviews};
+module.exports = { allReviews };
