7ced23175f6f5e041a6c840c51b1dc7ccc10c2a5
layabout <minyaeavmail@gmail.com>
2022-01-20 17:29:56 +0300

    qwe123

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -29,7 +29,7 @@ function App() {
         <BrowserRouter>
           <ScrollToTop />
           <Navigation />
-          <div className="mainContainer">
+          <div className="mainContainer"> 
 
             <Switch>
               <Route path="/" exact component={Home} />
